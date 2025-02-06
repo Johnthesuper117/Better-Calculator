ca0c5b1c83520407ad2ba78ae805138ad368dc8c
Johnthesuper117 <radking12j@gmail.com>
2025-02-06 12:00:21 -0500

    Create mathKnowledge.py

---
+++ mathKnowledge.py
@@ -0,0 +1,10 @@
+import math
+
+wholeSquares = [1:1, 2:4, 3:9, 4:16, 5:25]
+
+def knowledge(x):
+  return x
+
+print("What would you like to learn?")
+action = input(f"")
+print(f"\n{knowledge(action)}")
