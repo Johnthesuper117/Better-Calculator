819aca76a3f4664fce752fd412b9a0c1e3f7e39d
Johnthesuper117 <radking12j@gmail.com>
2025-02-06 12:29:41 -0500

    Update mathKnowledge.py

--- mathKnowledge.py
+++ mathKnowledge.py
@@ -1,10 +1,18 @@
 import math
 
-wholeSquares = [1:1, 2:4, 3:9, 4:16, 5:25]
+perfectSquares = {1:1, 2:4, 3:9, 4:16, 5:25, 6:36, 7:49, 8:64, 9:81, 10:100, 11:121, 12:144}
+
+class know:
+    name
+    square: 1
+    prime: 2
 
 def knowledge(x):
-  return x
+    print(x.upper())
+    if x.upper() == "PERFECT SQUARE":
+        return perfectSquares
 
 print("What would you like to learn?")
-action = input(f"")
+action = input(f"Here is a list of actions:\nPerfect Square\n")
+print(know[square])
 print(f"\n{knowledge(action)}")
