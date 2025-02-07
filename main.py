7eddce00db6c86176a594baa2c46e79c6726eb54
Johnthesuper117 <radking12j@gmail.com>
2025-02-07 10:41:02 -0500

    Update main.py
    it works!

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-import calculate as c
-import mathKnowledge as mK
-
-action = input(f"Choose an action:\n\n - Calculate (1)\n\n - Learn (2)\n\n")
-if action.upper() == "CALCULATE" or action.upper() == "1":
-  print("Make sure to use ' ' to separate operators, brackets, and numbers. Use * for multiplication, / for division, ^ for powers, sqrt for square root and pi for Pi")    
-  problem = input("Enter math problem: \n")
-  print(f"{problem} = {str(c.solve(problem))}")
-elif action.upper() == "LEARN" or action.upper() == "2":
-  print("What would you like to learn?")
-  action = input(f"Here is a list of actions:\n\n - Perfect Square (1)\n\n - Fibonacci Sequence (2)\n\n")
-  print(f"\n{mK.knowledge(action)}")
-else:
-  print("Action does not exist")
+from calculate import calculate as c
+from mathKnowledge import mathKnowledge as mK
+action = ""
+while action.upper() != "END":
+  action = input(f"Choose an action:\n\n - Calculate (1)\n\n - Learn (2)\n\n - END (0)\n\n")
+  if action.upper() == "CALCULATE" or action.upper() == "1":
+   print("Make sure to use ' ' to separate operators, brackets, and numbers. Use * for multiplication, / for division, ^ for powers, sqrt for square root and pi for Pi")    
+   problem = input("Enter math problem: \n")
+   print(f"{problem} = {str(c.solve(problem))}")
+  elif action.upper() == "LEARN" or action.upper() == "2":
+    print("What would you like to learn?")
+    action = input(f"Here is a list of actions:\n\n - Perfect Square (1)\n\n - Fibonacci Sequence (2)\n\n")
+    print(f"\n{mK.knowledge(action)}")
+  elif action.upper() == "END" or action.upper() == "0":
+    print("Ending project")
+    break
+  else:
+    print("Action does not exist")
