971cda66d0229e102ab59aeecb6b507eb4f4f3e5
Johnthesuper117 <radking12j@gmail.com>
2025-02-07 09:25:20 -0500

    Update mathKnowledge.py

--- mathKnowledge.py
+++ mathKnowledge.py
@@ -1,10 +1,13 @@
 import math
 
-class knowledge:
+def knowledge(what):
     perfectSquares = {1:1, 2:4, 3:9, 4:16, 5:25, 6:36, 7:49, 8:64, 9:81, 10:100, 11:121, 12:144}
-    self.PS = perfectSquares
+    
+    if what.upper() == 'PERFECT SQUARE':
+        return perfectSquares
+    elif 
 
 
 print("What would you like to learn?")
-action = input(f"Here is a list of actions:\nPerfect Square\n")
-print(f"\n{knowledge.action}")
+action = input(f"Here is a list of actions:\n\n - Perfect Square\n\n - Fibonacci Sequence")
+print(f"\n{knowledge(action)}")
