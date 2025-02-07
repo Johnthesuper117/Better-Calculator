4698c486b6db54984b02a0ca9ba1218bc44384fd
Johnthesuper117 <radking12j@gmail.com>
2025-02-07 10:20:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ action = input(f"Choose an action:\n\n - Calculate (1)\n\n - Learn (2)\n\n")
 if action.upper() == "CALCULATE" or action.upper() == "1":
   print("Make sure to use ' ' to separate operators, brackets, and numbers. Use * for multiplication, / for division, ^ for powers, sqrt for square root and pi for Pi")    
   problem = input("Enter math problem: \n")
-  print(problem + " = " + str(solve(problem)))
+  print(f"{problem} = {str(c.solve(problem))}")
 elif action.upper() == "LEARN" or action.upper() == "2":
   print("What would you like to learn?")
   action = input(f"Here is a list of actions:\n\n - Perfect Square (1)\n\n - Fibonacci Sequence (2)\n\n")
-  print(f"\n{knowledge(action)}")
+  print(f"\n{mK.knowledge(action)}")
+else:
+  print("Action does not exist")
