e7d635436aaa2abd2e79b1f4f6b8743c6ab8c5b7
Johnthesuper117 <radking12j@gmail.com>
2025-02-07 09:44:11 -0500

    Update mathKnowledge.py

--- mathKnowledge.py
+++ mathKnowledge.py
@@ -3,11 +3,18 @@ import math
 def knowledge(what):
     perfectSquares = {1:1, 2:4, 3:9, 4:16, 5:25, 6:36, 7:49, 8:64, 9:81, 10:100, 11:121, 12:144}
     
-    if what.upper() == 'PERFECT SQUARE':
+    if what.upper() == 'PERFECT SQUARE' or what.upper() == '1':
         return perfectSquares
-    elif 
+    elif what.upper() == 'FIBONACCI SEQUENCE' or what.upper() == '2':
+        t = int(input("How many numbers in the Sequence do you whant to see?\n"))
+        FibonacciSequence = [0, 1]
+        while int(t) > 0:
+            FibonacciSequence.append(FibonacciSequence[-1] + FibonacciSequence[-2])
+            t -= 1
+        print(f"{FibonacciSequence}")
+        
 
 
 print("What would you like to learn?")
-action = input(f"Here is a list of actions:\n\n - Perfect Square\n\n - Fibonacci Sequence")
+action = input(f"Here is a list of actions:\n\n - Perfect Square (1)\n\n - Fibonacci Sequence (2)\n\n")
 print(f"\n{knowledge(action)}")
