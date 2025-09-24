22b5cf9a2ad2f952ffc8683f37ab4068d63f4fcb
Johnthesuper117 <195603431+Johnthesuper117@users.noreply.github.com>
2025-09-24 18:27:14 +0000

    comandSyntax file

--- main.py
+++ main.py
@@ -4,25 +4,43 @@ import os
 import sys
 import json
 
-usercommand = ""
+if not os.path.exists("commands.json"):
+  print("Error: commands.json file not found.")
+  sys.exit(1)
 
 jsonFile = open("commands.json", "r")
 commands = json.load(jsonFile)
 
-"""
-while usercommand.upper() != "END":
-  action = input(f"Choose an action:\n\n - Calculate (1)\n\n - Learn (2)\n\n - END (0)\n\n")
-  if action.upper() == "CALCULATE" or action.upper() == "1":
-   print("Make sure to use ' ' to separate operators, brackets, and numbers. Use * for multiplication, / for division, ^ for powers, sqrt for square root and pi for Pi")    
-   problem = input("Enter math problem: \n")
-   print(f"{problem} = {eval(problem)}")
-  elif action.upper() == "LEARN" or action.upper() == "2":
-    print("What would you like to learn?")
-    action = input(f"Here is a list of actions:\n\n - Perfect Square (1)\n\n - Fibonacci Sequence (2)\n\n")
-    print(f"\n{mK.knowledge(action)}")
-  elif action.upper() == "END" or action.upper() == "0":
-    print("Ending project")
-    break
-  else:
-    print("Action does not exist")
-    """
+usercommand = ""
+
+if usercommand.upper() == "CALCULATE":
+  print("Make sure to use ' ' to separate operators, brackets, and numbers. Use * for multiplication, / for division, ^ for powers, sqrt for square root and pi for Pi")    
+  problem = input("Enter math problem: \n")
+  print(f"{problem} = {c.calculate(problem)}")
+elif usercommand.upper() == "LEARN":
+  print("What would you like to learn?")
+  action = input(f"Here is a list of actions:\n\n - Perfect Square (1)\n\n - Fibonacci Sequence (2)\n\n")
+  print(f"\n{mK.knowledge(action)}")
+elif usercommand.upper() == "END":
+  print("Ending project")
+  sys.exit(0)
+else:
+  print("Action does not exist")
+
+
+
+#while usercommand.upper() != "END":
+#  action = input(f"Choose an action:\n\n - Calculate (1)\n\n - Learn (2)\n\n - END (0)\n\n")
+#  if action.upper() == "CALCULATE" or action.upper() == "1":
+#   print("Make sure to use ' ' to separate operators, brackets, and numbers. Use * for multiplication, / for division, ^ for powers, sqrt for square root and pi for Pi")    
+#   problem = input("Enter math problem: \n")
+#   print(f"{problem} = {eval(problem)}")
+#  elif action.upper() == "LEARN" or action.upper() == "2":
+#    print("What would you like to learn?")
+#    action = input(f"Here is a list of actions:\n\n - Perfect Square (1)\n\n - Fibonacci Sequence (2)\n\n")
+#    print(f"\n{mK.knowledge(action)}")
+#  elif action.upper() == "END" or action.upper() == "0":
+#    print("Ending project")
+#    break
+#  else:
+#    print("Action does not exist")
